1c5fedcd6ae69521708528fc2e0c1124aead70a9
butterscotchh <sampansedang@gmail.com>
2025-09-23 02:00:26 +0700

    booking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,24 @@ import './App.css';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
-//ROUTER
-import { Routes, Route } from 'react-router-dom'
+// ROUTER
+import { Routes, Route } from 'react-router-dom';
 
-//IMPORT SEMUA PAGE DISINI
-import Home from './pages/home.jsx'
-import Register from './pages/register.jsx'
-import Login from './pages/login.jsx'
+// IMPORT SEMUA PAGE DISINI
+import Home from './pages/home.jsx';
+import Register from './pages/register.jsx';
+import Login from './pages/login.jsx';
+import Book from './pages/book.jsx';  // <-- Tambahin import Book
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/register" element={<Register/>}></Route>
-      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/book" element={<Book />} /> {/* Routing ke Book */}
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
