58746c983ef9a77c7da6933f1ac3783f0c0b6e45
Ciel806 <ceryne688@gmail.com>
2025-09-27 22:26:32 +0700

    idk la

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -28,7 +28,6 @@ export default function HomePage() {
     <div className="w-full h-auto overflow-x-hidden">
       <NavigationBar openMenu={openMenu} handleOpenMenu={handleOpenMenu} />
 
-      {/* Fixed Book button */}
       <div className="fixed top-5 right-8 z-50">
         <Link
           to="/booking"
@@ -38,7 +37,6 @@ export default function HomePage() {
         </Link>
       </div>
 
-      {/* Hero video */}
       <div className="w-full h-screen relative overflow-hidden">
         <video
           src="https://lvmh-chevalblanc.cdn.prismic.io/lvmh-chevalblanc/aCcrMCdWJ-7kSN-l_PaysageCBO.mp4"
@@ -50,23 +48,21 @@ export default function HomePage() {
         ></video>
       </div>
 
-      {/* Rooms Section */}
-      <section id="rooms" className="bg-[#fbfaf9] font-serif px-0 py-20">
-        <h2 className="text-center text-3xl font-bold text-[#333] mb-4">
+      <section id="rooms" className="bg-[#fbfaf9] px-0 py-20">
+        <h2 className="text-center text-4xl font-bold text-[#333] mb-4">
           Rooms & Suites
         </h2>
-        <h3 className="text-center text-lg text-[#666] mb-10">
+        <h3 className="text-center text-xl text-[#666] mb-10">
           A range of accommodations from intimate suites to private penthouses.
           Each room carefully designed for comfort and alpine views.
         </h3>
         <RoomDisplay />
       </section>
 
-      {/* Dining Section */}
-      <div id="dine" className="px-8 py-20 bg-white">
+      <div id="dine" className="px-8 py-20 bg-[#fbfaf9]">
         <div className="text-center mb-8">
-          <h2 className="text-3xl font-bold text-[#333] mb-4">Dine With Us</h2>
-          <h3 className="text-lg text-[#666]">
+          <h2 className="text-4xl font-bold text-[#333] mb-4">Dine With Us</h2>
+          <h3 className="text-xl text-[#666]">
             Experience culinary excellence at our on-site restaurants and bars,
             offering a variety of gourmet dishes and drinks.
           </h3>
@@ -74,7 +70,6 @@ export default function HomePage() {
         <Carousel />
       </div>
 
-      {/* Floating Book Now Button */}
       {showFloating && (
         <div className="fixed bottom-6 right-6 z-50">
           <Link
