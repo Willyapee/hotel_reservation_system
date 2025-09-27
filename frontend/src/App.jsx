3c495fce3b7a2493b30122aa4b15fcd855a4d5d8
butterscotchh <sampansedang@gmail.com>
2025-09-27 22:09:36 +0700

    ceryneee-cepatt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,20 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
-import { Link } from 'react-router-dom';
-
-
-import HomePage from './pages/homePage.jsx';
-import MenuPage from './pages/menuPage.jsx';
-import Register from './pages/register.jsx';
-import Login from './pages/Login.jsx';
-import NavigationBar from './components/navigationBar.jsx';
-
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/homePage.jsx";
+import MenuPage from "./pages/menuPage.jsx";
+import Register from "./pages/register.jsx";
+import Login from "./pages/Login.jsx";
+import Book from "./pages/book.jsx";
 
 function App() {
-
-	return (
-		<>
-			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/menu' element={<MenuPage />} />
-				<Route path='/register' element={<Register />} />
-				<Route path='/login' element={<Login />} />
-			
-			</Routes>
-		</>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/menu" element={<MenuPage />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/booking" element={<Book />} />
+    </Routes>
+  );
 }
 
 export default App;
