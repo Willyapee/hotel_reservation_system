49d9f7c3d686c8213e756dba0a3c37a91c467619
Lnyx90 <lnyx90jeu@gmail.com>
2025-10-09 23:13:50 +0700

    udah nih will

--- backend/models/ServiceReservations.js
+++ backend/models/ServiceReservations.js
@@ -1,36 +1,22 @@
-import { DataTypes } from "sequelize";
-import db from "../config/db.js";
+//Import Libraries
+import { DataTypes } from 'sequelize';
 
-const BookingRoomFacility = db.define("booking_room_facility", {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    id_facility: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    id_booking_room: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    unit_price: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false
-    },
-    qty: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 1
-    },
-    subtotal: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false
-    }
-}, {
-    freezeTableName: true,
-    timestamps: false
+//Import Database
+import db from '../config/db.js';
+
+//Create Service Reservations Model
+const ServiceReservations = db.define('service_reservations', {
+	id_service_reservation: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true,
+	},
+	quantity: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+	},
+	subtotal_price: {
+		type: DataTypes.DECIMAL(63, 2),
+		allowNull: false,
+	},
 });
-
-export default BookingRoomFacility;
