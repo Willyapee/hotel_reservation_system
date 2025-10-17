83c36d6212b90a6a25d7dd1e108771fe4e4ea389
Willyapee <williampurnamadjaja2424@gmail.com>
2025-10-17 23:44:56 +0700

    Register fix

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,11 +1,11 @@
-import User from '../models/MsUsers.js';
+import MsUser from '../models/MsUsers.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 const salt = 10;
 
 export const registerUser = async (req, res) => {
-	const { name, email, password } = req.body;
+	const { username, email, password } = req.body;
 	try {
 		//Express menangkap axios.post via req.bodu
 
@@ -14,7 +14,7 @@ export const registerUser = async (req, res) => {
     findOne = ambil satu baris data dari database.
     { where: { email } } = kondisi query → ambil user dengan kolom email sama dengan nilai email yang dikirim dari frontend.
     */
-		const existingUser = await User.findOne({ where: { email } });
+		const existingUser = await MsUser.findOne({ where: { email } });
 		if (existingUser) {
 			//status 400 = User exist already
 			return res.status(400).json({ message: 'User already exists' });
@@ -22,15 +22,15 @@ export const registerUser = async (req, res) => {
 
 		const hashedPassword = await bcrypt.hash(password, salt);
 
-		const newUser = await User.create({
-			name,
+		const newUser = await MsUser.create({
+			username,
 			email,
 			password: hashedPassword,
 		});
 
 		res.status(201).json({
 			message: 'User registered successfully',
-			user: { id: newUser.id, name: newUser.name, email: newUser.email },
+			user: { id: newUser.id_user, username: newUser.username, email: newUser.email },
 		});
 	} catch (error) {
 		console.log(error);
@@ -48,18 +48,18 @@ export const loginUser = async (req, res) => {
 		}
 
 		//cek user email udh bener/blm
-		const user = await User.findOne({ where: { email } });
+		const user = await MsUser.findOne({ where: { email } });
 		if (!user) return res.status(400).json({ message: 'Invalid email or password' });
 
 		console.log('login attempt:', email, password);
 		console.log('stored hash:', user.password);
 
-		//cek password
+		//cek passiword
 		const isMatch = await bcrypt.compare(password, user.password); //compare(plainText, hashed)
 		if (!isMatch) return res.status(400).json({ message: 'Invalid email or password' });
 
 		//generate token
-		const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
+		const token = jwt.sign({ id: user.id_user, email: user.email }, process.env.JWT_SECRET, {
 			expiresIn: '1h',
 		});
 
@@ -90,12 +90,16 @@ export const getMe = async (req, res) => {
 		if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
 
 		const decoded = jwt.verify(token, process.env.JWT_SECRET);
-		const user = await User.findByPk(decoded.id, {
-			attributes: ['id', 'name', 'email'], // exclude password
+		const user = await MsUser.findByPk(decoded.id, {
+			attributes: ['id_user', 'username', 'email'], // exclude password
 		});
 		if (!user) return res.status(404).json({ message: 'User not found' });
 
-		res.status(200).json(user);
+		res.status(200).json({
+			id: user.id_user,
+			username: user.username,
+			email: user.email,
+		});
 	} catch (error) {
 		console.log(error);
 		res.status(401).json({ message: 'Invalid token' });
