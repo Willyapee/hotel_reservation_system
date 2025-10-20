09e4f7583594982702510269d9b1cb12b2140ef7
Lnyx90 <lnyx90jeu@gmail.com>
2025-10-21 05:01:02 +0700

    .

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -56,7 +56,7 @@ const Login = () => {
             {/* Video Background */}
             <div className="absolute inset-0 w-full h-full">
                 <video
-                    src="/video/ChevalBlanc.mp4"
+                    src="/video/Resort.mp4"
                     className="h-full w-full object-cover"
                     autoPlay
                     loop
