a7d98515c0c93088988e454d9d028733862b6c7b
Willyapee <williampurnamadjaja2424@gmail.com>
2025-10-21 03:40:11 +0700

    Reset .env ama config db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -16,14 +16,7 @@ const db = new Sequelize(
 	//addition
 	{
 		host: process.env.DB_HOST,
-		port: process.env.DB_PORT,
 		dialect: 'mysql', //jenis database engine yg dipake sequelize
-		dialectOptions: {
-			//Wajib utk AIVEN
-			ssl: {
-				rejectUnauthorized: true,
-			}
-		}
 	}
 );
 
